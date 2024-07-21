530afb04ba694e7aade34c49c4ca5c62edc93eec
Nethsara Apium <nethsara@apium.io>
2024-07-21 13:07:43 +0530

    Init Code

---
+++ session-2/src/components/appbar/AppBar.jsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import './AppBar.css'
+
+export const AppBar: FC = () => {
+    return (
+        <div className="app-bar">
+            <img src="solana.png" alt="solona logo" />
+            <div className="wallet-button">
+                <WalletMultiButton
+                    style={{
+                        backgroundColor: '#2a51df',
+                        color: '#fff',
+                        padding: '10px 20px',
+                        border: 'none',
+                        fontSize: '16px',
+                        cursor: 'pointer',
+                        width:  '178px',
+                        height: '58px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        transition: 'background-color 0.3s ease',
+                    }}
+                    hoverStyle={{
+                        backgroundColor: '#3f44c2',
+                    }}
+                />
+            </div>
+        </div>
+    )
+}
