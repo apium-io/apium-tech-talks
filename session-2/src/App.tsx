e5fd291cf8bd54476a199708f10d798264f33161
jinali98 <68109485+jinali98@users.noreply.github.com>
2024-08-05 08:51:30 +0530

    updated codebase with code modifications and comments

--- session-2/src/App.tsx
+++ session-2/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './App.css';
 import WalletContextProvider from "./components/WalletContextProvider";
 import {AppBar} from "./components/AppBar/AppBar";
--- session-2/src/components/WalletContextProvider.tsx
+++ session-2/src/components/WalletContextProvider.tsx
@@ -1,23 +1,31 @@
-import { FC, ReactNode } from 'react';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { FC, ReactNode } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
-import * as web3 from '@solana/web3.js'
-import * as walletAdapterWallets from '@solana/wallet-adapter-wallets';
-require('@solana/wallet-adapter-react-ui/styles.css');
+import * as web3 from "@solana/web3.js";
+import * as walletAdapterWallets from "@solana/wallet-adapter-wallets";
+require("@solana/wallet-adapter-react-ui/styles.css");
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
-    const endpoint = web3.clusterApiUrl('testnet')
-    const wallets = [new walletAdapterWallets.PhantomWalletAdapter()]
+  // endpoint of the cluster to connect to
+  const endpoint = web3.clusterApiUrl("testnet");
 
-    return (
-        <ConnectionProvider endpoint={endpoint}>
-            <WalletProvider wallets={wallets}>
-                <WalletModalProvider>
-                    { children }
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-    )
-}
+  // list of wallets to be used
+  const wallets = [
+    new walletAdapterWallets.PhantomWalletAdapter(),
+    new walletAdapterWallets.CoinbaseWalletAdapter(),
+    new walletAdapterWallets.SolflareWalletAdapter(),
+  ];
 
-export default WalletContextProvider
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
